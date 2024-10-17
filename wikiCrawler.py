6fa8c5eae1bb702fb080dc6acf2f702d881e280e
Jesse Zhou <zzywasd@gmail.com>
2024-10-16 20:06:54 -0700

    wikiCrawler file modified.

--- wikiCrawler.py
+++ wikiCrawler.py
@@ -5,8 +5,12 @@ def cap_string(input_string):
     result = input_string.title().replace(" ", "_")
     return result
 
+user_input = input("Enter a name to search: ")
 
-input = "simon fraser university"
+print("Searching... ", user_input)
+
+input = user_input
+
 suffix = cap_string(input)
 
 url = "https://en.wikipedia.org/wiki/"
@@ -25,7 +29,10 @@ if table:
         header = row.find('th')  
         data = row.find('td')  
         if header and data:
-            print(f"{header.text.strip()}: {data.text.strip()}")
+            print(f"{header.text.strip()}: {data.text.strip()}") # comment this out later
+            if header.text.strip() == "Website":
+                target_website = data.text.strip()
+
 else:
     url_query = url+input+"_(company)"
     response = requests.get(url_query)
@@ -40,6 +47,12 @@ else:
             header = row.find('th')  
             data = row.find('td')  
             if header and data:
-                print(f"{header.text.strip()}: {data.text.strip()}")
+                print(f"{header.text.strip()}: {data.text.strip()}") # comment this out later
+                if header.text.strip() == "Website":
+                    target_website = data.text.strip()
     else:
         print("Table not found.")
+
+print(target_website) #comment this out later
+
+# use "target_website" as the variable name in following scripts 
