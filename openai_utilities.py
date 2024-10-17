39428828fc9c388e32ee10479b157973c1853e6a
Jesse Zhou <zzywasd@gmail.com>
2024-10-16 23:39:20 -0700

    Modified openai_utilities, main.py

--- openai_utilities.py
+++ openai_utilities.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 def openai_query(query):
-    client = OpenAI(api_key='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')
+    client = OpenAI(api_key='##key here##')
 
     completion = client.chat.completions.create(
     model="gpt-4o-mini",
